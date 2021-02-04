24f45a020b0c3658e94c77ea10550db46a9e9916
N-Harish <harishnatarajan24@gmail.com>
2021-02-04 21:27:03 +0530

    Test folder created

--- test_api.py
+++ test_api.py
@@ -2,11 +2,16 @@ import base64
 import requests
 
 
-with open(r"test2.jpg", 'rb') as image_string:
+with open(r"test_images/test2.jpg", 'rb') as image_string:
     byte_string = base64.b64encode(image_string.read()).decode('utf-8')
+
+# with open(r"../appa.jpg", 'rb') as image_string:
+#     byte_string = base64.b64encode(image_string.read()).decode('utf-8')
 
 
 res = requests.post('http://127.0.0.1:8000/predict', json={'data': byte_string})
+
+# res = requests.post('https://age-restrict-content-provider.herokuapp.com/predict', json={'data': byte_string})
 
 print(res.json())
 
