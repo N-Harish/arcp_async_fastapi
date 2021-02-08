9f9390bc7b45eef1ae6593be49e45716b6351d0f
N-Harish <harishnatarajan24@gmail.com>
2021-02-08 18:44:33 +0530

    Updated Model

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import uvicorn
 from PIL import Image
 from fastapi import FastAPI
 from arcp import ARCP
-
 from typing import List
 import onnxruntime
 
 
+# preprocess data
 async def get_data(image_path: str) -> List[int]:
     """
     Args:
@@ -19,14 +19,13 @@ async def get_data(image_path: str) -> List[int]:
         data (list): list of image pixel of size (1, 224, 224, 3)
     """
     img = Image.open(BytesIO(base64.b64decode(image_path))).convert('RGB')
-    # img = Image.frombytes("RGB", (224, 224))
-    # img = Image.open(image_path)
     img = img.resize((224, 224))
     img1 = np.array(img)
     data = np.expand_dims(img1, axis=0)
     return list(data)
 
 
+# load model and generate prediction
 async def get_prediction(data: List[int]) -> int:
     """
     Args:
@@ -36,11 +35,10 @@ async def get_prediction(data: List[int]) -> int:
         age (int): returns predicted age
     """
 
-    session = onnxruntime.InferenceSession('onnx_arcp_v1.onnx')
+    session = onnxruntime.InferenceSession('resnet50_age_detector.onnx')
     input_name = session.get_inputs()[0].name
     result = session.run(None, {input_name: data})
     age = round(result[0][0][0])
-    # print(f'{age=}')
     return age
 
 
@@ -55,7 +53,6 @@ def welcome():
 @app.post('/predict')
 async def predict(data: ARCP):
     data = data.dict()
-    # print(data)
     image_name = data['data']
     dataset = await get_data(image_name)
     prediction = await get_prediction(dataset)
--- test_api.py
+++ test_api.py
@@ -2,7 +2,7 @@ import base64
 import requests
 
 
-with open(r"test_images/test2.jpg", 'rb') as image_string:
+with open(r"test_images/omkar.png", 'rb') as image_string:
     byte_string = base64.b64encode(image_string.read()).decode('utf-8')
 
 # with open(r"../appa.jpg", 'rb') as image_string:
@@ -11,6 +11,7 @@ with open(r"test_images/test2.jpg", 'rb') as image_string:
 
 res = requests.post('http://127.0.0.1:8000/predict', json={'data': byte_string})
 
+print(byte_string)
 # res = requests.post('https://age-restrict-content-provider.herokuapp.com/predict', json={'data': byte_string})
 
 print(res.json())
